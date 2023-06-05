318aac4512fac14e0ffc762ce0fe9bff55fc0b8e
ahracho <ahra123@i-commtech.com>
2023-06-05 13:37:06 +0900

    securityconfig add

---
+++ src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity  //웹보안이 활성화되는 어노테이션
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                );
+        http //우리가 원하는 id/pw 로그인 되도록 설정
+                .formLogin();
+        return http.build();
+    }
+}
